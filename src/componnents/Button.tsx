8e25afe4e51d12d28e911c3c49975cb3fdf26e98
Andriantsitohaina <andriantsitohaina.luca@gmail.com>
2025-12-14 21:52:04 +0300

    changing title

--- src/componnents/Button.tsx
+++ src/componnents/Button.tsx
@@ -1,29 +1,22 @@
-interface props{
-    text:string
-    className:string
-    handleClick?:()=>void
+interface props {
+  text: string;
+  className: string;
+  handleClick?: () => void;
 }
-import arrowd from "../assets/images/arrowd.svg"
+import arrowd from "../assets/images/arrowd.svg";
 
-const Button = ({text,className,handleClick}:props) => {
+const Button = ({ text, className, handleClick }: props) => {
   return (
-    
-    <a
-    
-    className={ `${className ?? "" } cta-wrapper`}>
-        <div 
-        onClick={handleClick}
-        className="cta-button group">
-            <div className="bg-circle"/>
-            <p className="text">{text}</p>
-            <div className="arrow-wrapper">
-            <img src={arrowd} alt="arrow" />
-            </div>
-            
+    <a className={`${className ?? ""} cta-wrapper`}>
+      <div onClick={handleClick} className="cta-button group">
+        <div className="bg-circle" />
+        <p className="text">{text}</p>
+        <div className="arrow-wrapper">
+          <img src={arrowd} alt="arrow" />
         </div>
-
+      </div>
     </a>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
--- src/layout/Showcase.tsx
+++ src/layout/Showcase.tsx
@@ -1,82 +1,82 @@
 import { useRef } from "react";
 import mangaddict from "../assets/images/mangaddict.png";
-import Mosary from "../assets/images/Mosary.png"
-import project3 from "../assets/images/project3.png"
-import {gsap} from 'gsap';
-import {ScrollTrigger} from 'gsap/ScrollTrigger';
+import Mosary from "../assets/images/Mosary.png";
+import project3 from "../assets/images/project3.png";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
 const Showcase = () => {
-  const sectionref=useRef(null)
-  const Mosaryctref=useRef(null)
-  const mangaddictctref=useRef(null)
-  const project3ref=useRef(null)
- 
+  const sectionref = useRef(null);
+  const Mosaryctref = useRef(null);
+  const mangaddictctref = useRef(null);
+  const project3ref = useRef(null);
 
-  
-  useGSAP(()=>{
-    const projects=[Mosaryctref.current,mangaddictctref.current,project3ref.current]
-    
+  useGSAP(() => {
+    const projects = [
+      Mosaryctref.current,
+      mangaddictctref.current,
+      project3ref.current,
+    ];
 
-    gsap.fromTo(sectionref.current, {
-      opacity:0
-    }, 
-    {
-      opacity:1,
-      duration:1.5,
-      scrollTrigger: {
-        trigger: sectionref.current,
-        start: 'top bottom-=100',
-        toggleActions: "play none none reverse"
-      }
-    })
-    projects.forEach((card,index)=>{
     gsap.fromTo(
-      card,{
-        y:50,
-        opacity:0
+      sectionref.current,
+      {
+        opacity: 0,
       },
       {
-        y:0,
-        opacity:1,
-        duration:1,
-        delay:0.3 * (index+1),
-        scrollTrigger:{
-          trigger:card,
-          start:'top bottom-=100',
-          toggleActions: "play none none reverse"
-        }
+        opacity: 1,
+        duration: 1.5,
+        scrollTrigger: {
+          trigger: sectionref.current,
+          start: "top bottom-=100",
+          toggleActions: "play none none reverse",
+        },
       }
-    )
-  }) 
-  },[])
+    );
+    projects.forEach((card, index) => {
+      gsap.fromTo(
+        card,
+        {
+          y: 50,
+          opacity: 0,
+        },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 1,
+          delay: 0.3 * (index + 1),
+          scrollTrigger: {
+            trigger: card,
+            start: "top bottom-=100",
+            toggleActions: "play none none reverse",
+          },
+        }
+      );
+    });
+  }, []);
 
   return (
     <section id="work" ref={sectionref} className="app-showcase">
       <div className="w-full">
         <div className="showcaselayout">
-          ²
-          {/* left*/}
-          <div className="first-project-wrapper" ref={Mosaryctref}>
-            <div className="image-wrapper">
-              <img
-                src={Mosary}
-                alt="Mosary"
-                className=""
-              />
+          ²{/* left*/}
+          <a href="https://mosary.vercel.app/">
+            <div className="first-project-wrapper" ref={Mosaryctref}>
+              <div className="image-wrapper">
+                <img src={Mosary} alt="Mosary" className="" />
+              </div>
+              <div className="text-content">
+                <h2>Mosary is a recipe finder</h2>
+                <p className="text-white-50 md:tex-xl">
+                  An app built in react ,tailwindcss ,The mealdb as an api,load
+                  times are optimized and and the interface is user friendly
+                </p>
+              </div>
             </div>
-            <div className="text-content">
-              <h2>
-                Mosary is a recipe finder
-              </h2>
-              <p className="text-white-50 md:tex-xl">
-                An app built in react ,tailwindcss ,The mealdb as an api,load times are optimized and and the interface is user friendly 
-              </p>
-            </div>
-          </div>
-
+          </a>
           {/* right*/}
           <div className="project-list-wrapper overflow-hidden">
             <div className="project" ref={mangaddictctref}>
@@ -88,14 +88,10 @@ const Showcase = () => {
             <div className="project" ref={project3ref}>
               <div className="image-wrapper bg-amber-100">
                 <img src={project3} alt="YC directory" />
-
               </div>
               <h2>Startup Showcase App</h2>
             </div>
-          
-
           </div>
-
         </div>
       </div>
     </section>
